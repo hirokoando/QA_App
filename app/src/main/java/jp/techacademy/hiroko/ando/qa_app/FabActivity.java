d5685d8038f89a7456cf9ef8f516f88237be3b3c
hirokoando <01074ahinrodoko4@gmail.com>
2018-11-08 21:33:17 +0900

    Lesson8　課題４～６
    お気に入り一覧完成

--- app/src/main/java/jp/techacademy/hiroko/ando/qa_app/FabActivity.java
+++ app/src/main/java/jp/techacademy/hiroko/ando/qa_app/FabActivity.java
@@ -2,9 +2,6 @@ package jp.techacademy.hiroko.ando.qa_app;
 
 
 import android.content.Intent;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,20 +19,22 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class FabActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
-    private int mGenre = 1;
-
     private DatabaseReference mDatabaseReference;
-    private DatabaseReference mFabRef;
+    private DatabaseReference mFavRef;
+    private DatabaseReference mContentRef;
     private ListView mListView;
     private ArrayList<Question> mQuestionArrayList;
     private QuestionsListAdapter mAdapter;
+    private ArrayList<String> FavIdList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +57,6 @@ public class FabActivity extends AppCompatActivity {
         mQuestionArrayList = new ArrayList<Question>();
         mAdapter.notifyDataSetChanged();
 
-
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -68,72 +66,55 @@ public class FabActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
+        FavIdList = new ArrayList<>();
         // 質問のリストをクリアしてから再度Adapterにセットし、AdapterをListViewにセットし直す
         mQuestionArrayList.clear();
         mAdapter.setQuestionArrayList(mQuestionArrayList);
         mListView.setAdapter(mAdapter);
 
         // 選択したジャンルにリスナーを登録する
-        if (mFabRef != null) {
-            mFabRef.removeEventListener(mEventListener);
+        if (mFavRef != null) {
+            mFavRef.removeEventListener(mFavListener);
         }
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        mFabRef = mDatabaseReference.child(Const.FavoritesPATH).child(user.getUid());
-        mFabRef.addChildEventListener(mEventListener);
+        mFavRef = mDatabaseReference.child(Const.FavoritesPATH).child(user.getUid());
+        mFavRef.addChildEventListener(mFavListener);
+
     }
 
-    private ChildEventListener mEventListener = new ChildEventListener() {
+    private ChildEventListener mFavListener = new ChildEventListener() {
         @Override
         public void onChildAdded(DataSnapshot dataSnapshot, String s) {
             HashMap map = (HashMap) dataSnapshot.getValue();
-            String title = (String) map.get("title");
-            String body = (String) map.get("body");
-            String name = (String) map.get("name");
-            String uid = (String) map.get("uid");
-            String imageString = (String) map.get("image");
-            byte[] bytes;
+            final String genre =(String)map.get("Genre");
+            String qestionuid = dataSnapshot.getKey();
 
-            if (imageString != null) {
-                bytes = Base64.decode(imageString, Base64.DEFAULT);
-            } else {
-                bytes = new byte[0];
-            }
+            mContentRef = mDatabaseReference.child(Const.ContentsPATH).child(genre).child(qestionuid);
 
-            ArrayList<Answer> answerArrayList = new ArrayList<Answer>();
-            HashMap answerMap = (HashMap) map.get("answers");
-            if (answerMap != null) {
-                for (Object key : answerMap.keySet()) {
-                    HashMap temp = (HashMap) answerMap.get((String) key);
-                    String answerBody = (String) temp.get("body");
-                    String answerName = (String) temp.get("name");
-                    String answerUid = (String) temp.get("uid");
-                    Answer answer = new Answer(answerBody, answerName, answerUid, (String) key);
-                    answerArrayList.add(answer);
-                }
-            }
+            mContentRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot snapshot) {
+                    HashMap map = (HashMap) snapshot.getValue();
+                    String title = (String) map.get("title");
+                    String body = (String) map.get("body");
+                    String name = (String) map.get("name");
+                    String uid = (String) map.get("uid");
+                    String imageString = (String) map.get("image");
+                    byte[] bytes;
+                    if (imageString != null) {
+                        bytes = Base64.decode(imageString, Base64.DEFAULT);
+                    } else {
+                        bytes = new byte[0];
+                    }
 
-            Question question = new Question(title, body, name, uid, dataSnapshot.getKey(), mGenre, bytes, answerArrayList);
-            mQuestionArrayList.add(question);
-            mAdapter.notifyDataSetChanged();
-        }
-
-        @Override
-        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-            HashMap map = (HashMap) dataSnapshot.getValue();
-
-            // 変更があったQuestionを探す
-            for (Question question: mQuestionArrayList) {
-                if (dataSnapshot.getKey().equals(question.getQuestionUid())) {
-                    // このアプリで変更がある可能性があるのは回答(Answer)のみ
-                    question.getAnswers().clear();
+                    ArrayList<Answer> answerArrayList = new ArrayList<Answer>();
                     HashMap answerMap = (HashMap) map.get("answers");
                     if (answerMap != null) {
                         for (Object key : answerMap.keySet()) {
@@ -142,13 +123,22 @@ public class FabActivity extends AppCompatActivity {
                             String answerName = (String) temp.get("name");
                             String answerUid = (String) temp.get("uid");
                             Answer answer = new Answer(answerBody, answerName, answerUid, (String) key);
-                            question.getAnswers().add(answer);
+                            answerArrayList.add(answer);
                         }
                     }
 
+                    Question question = new Question(title, body, name, uid, snapshot.getKey(), Integer.parseInt(genre), bytes, answerArrayList);
+                    mQuestionArrayList.add(question);
                     mAdapter.notifyDataSetChanged();
                 }
-            }
+                @Override
+                public void onCancelled(DatabaseError firebaseError) {
+                }
+            });
+        }
+
+        @Override
+        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
         }
 
         @Override
@@ -166,6 +156,7 @@ public class FabActivity extends AppCompatActivity {
 
         }
     };
+
 
     //戻るボタン
     @Override
